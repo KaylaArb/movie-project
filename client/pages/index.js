6dc98e9e2a1dbf51a1a25aa00952f7be64ee39bc
KaylaArb <35358725+KaylaArb@users.noreply.github.com>
2021-01-13 21:24:26 +0100

    frontend - fixed fetch functions, added loader

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,36 +1,42 @@
 import Head from 'next/head'
-import Link from "next/link";
 import styles from '../styles/Home.module.css'
 import Footer from "../components/Footer";
 import MovieBlock from "../components/MovieBlock";
 import ReactPaginate from "react-paginate"
 import router from "next/router"
-import {useState} from 'react';
+import { useState } from 'react';
 import fetch from "node-fetch"
 
-export default function Home({shows, show}) {
+const backendUrl = "https://movie-application-backend.herokuapp.com/api/"
+
+export default function Home({ shows, show }) {
     const [searchValue, setSearchValue] = useState('');
     const [homePage, setHomePage] = useState(true);
-    const [forcePage, setForcePage] = useState(null);
-    let filters = [{name :"Choose a Filter...", value: null }, {name :"Top Rated", value:"high_rating" },{name :"Lowest Rated", value:"low_rating"}, {name:"Popularity", value:"popularity"} ]
-    let filterButtons = [{name :"Ascending", value:"ascending" }, {name :"Descending", value:"descending" }]
+    const [loading, setLoading] = useState(false)
+    let filters = [{ name: "Choose a Filter...", value: null }, { name: "Top Rated", value: "high_rating" }, { name: "Lowest Rated", value: "low_rating" }, { name: "Popularity", value: "popularity" }]
+    let filterButtons = [{ name: "Ascending", value: "ascending" }, { name: "Descending", value: "descending" }]
 
     const handlePagination = page => {
         const path = router.pathname
         const query = router.query
         query.page = page.selected + 1
-        if(query.page > 1) {
-            setHomePage(false)
+        if (query.page > 1) {
             router.push({
                 pathname: path,
                 query: query,
-            }).then(() => window.scrollTo(0, 0));
+            }).then(() => {
+                window.scrollTo(0, 0)
+                setHomePage(false)
+            });
 
         } else {
-            setHomePage(true)
             router.push({
-                    pathname: path}
-            ).then(() => window.scrollTo(0, 0));
+                    pathname: path
+                }
+            ).then(() => {
+                window.scrollTo(0, 0)
+                setHomePage(true)
+            });
         }
     }
 
@@ -50,12 +56,13 @@ export default function Home({shows, show}) {
         const path = router.pathname
         const query = router.query
         query.search = searchValue
-        setHomePage(false);
         router.push({
             pathname: path,
             query: query,
-        }).then(() => window.scrollTo(0, 0));
-        setSearchValue('')
+        }).then(() => {
+            setSearchValue('')
+            setHomePage(false);
+        })
     };
 
     const favourites = (value) => {
@@ -66,6 +73,23 @@ export default function Home({shows, show}) {
             pathname: path,
             query: query,
         }).then(() => setHomePage(false));
+    }
+
+    function filter() {
+        { setLoading(true) }
+        let value = document.getElementById("filter").value;
+        fetch(backendUrl + "filter/" + value)
+            .then(() => {
+                router.reload(window.location.pathname)
+            })
+    }
+
+    function buttonFilter(value) {
+        { setLoading(true) }
+        fetch(backendUrl + "filter/" + value)
+            .then(() => {
+                router.reload(window.location.pathname);
+            })
     }
 
     return (
@@ -83,7 +107,7 @@ export default function Home({shows, show}) {
                             className={styles.searchInput}
                             value={searchValue}
                             onKeyDown={handleKeyDown}
-                            onChange={handleChangeSearchValue}/>
+                            onChange={handleChangeSearchValue} />
                         {
                             searchValue && (
                                 <button
@@ -103,7 +127,7 @@ export default function Home({shows, show}) {
                     </div>
                     <div>
                         {filterButtons.map((filter) => (
-                            <button type="submit" value={filter.value} id="buttonFilter" onClick={() => buttonFilter(filter.value)} className={styles.filterButtons}>{filter.name === "Descending" ? <p>˅</p> : <p>˄</p>} </button>
+                            <button type="submit" onClick={() => buttonFilter(filter.value)} className={styles.filterButtons}>{filter.name === "Descending" ? <p>˅</p> : <p>˄</p>} </button>
                         ))}
                     </div>
                     <select id="filter" className={styles.selectInput} onChange={filter}>
@@ -115,67 +139,54 @@ export default function Home({shows, show}) {
                 <div className={`${styles.backHome} ${!homePage ? styles.active : ''}`}>
                     <a onClick={() => handlePagination(1)}>Back to Home Page</a>
                 </div>
-
-                <div className={styles.movieGrid}>
+                {loading ? <h1 className={styles.loader}>Loading..</h1> : <div> <div className={styles.movieGrid}>
                     {shows ? shows.results.map((show) => (
-                        <MovieBlock key={show.id} movie={show}/>
+                        <MovieBlock key={show.id} movie={show} />
                     )) : show.length < 1 ? <p className={styles.noResults}>No results found</p> : show.map((show) => (
-                        <MovieBlock key={show.id} movie={show}/>))
-                     }
+                        <MovieBlock key={show.id} movie={show} />))
+                    }
                 </div>
-                {show ? '' : <ReactPaginate
-                    marginPagesDisplayed={1}
-                    previousLabel={"Previous"}
-                    nextLabel={"Next"}
-                    initialPage={shows.page - 1}
-                    forcePage={shows.page - 1}
-                    pageCount={shows.total_pages}
-                    onPageChange={handlePagination}
-                    containerClassName={styles.pagination}
-                    activeClassName={styles.active}
-                    previousClassName={styles.previous}
-                    nextClassName={styles.next}
-                    pageLinkClassName={styles.page}
-                    disabledClassName={styles.disabled}
-                /> }
-
+                    {show ? '' : <ReactPaginate
+                        marginPagesDisplayed={1}
+                        previousLabel={"Previous"}
+                        nextLabel={"Next"}
+                        initialPage={shows.page - 1}
+                        forcePage={shows.page - 1}
+                        pageCount={shows.total_pages}
+                        onPageChange={handlePagination}
+                        containerClassName={styles.pagination}
+                        activeClassName={styles.active}
+                        previousClassName={styles.previous}
+                        nextClassName={styles.next}
+                        pageLinkClassName={styles.page}
+                        disabledClassName={styles.disabled}
+                    />}
+                </div>}
             </main>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
 
-export async function getServerSideProps({query}) {
+export async function getServerSideProps({ query }) {
     const page = query.page || 1
     const search = query.search || null
     const favourites = query.favourites || null
     console.log("this is page number = " + page)
     console.log("this is search result = " + search)
-    if(search === null && favourites === null) {
-        const shows = await fetch(`http://localhost:8080/api/page=${page}`).then(r => r.json())
-        return {props: {shows}}
-    } else if(search) {
-        const show = await fetch(`http://localhost:8080/api/search=${search}`).then(r => r.json())
-        return {props: {show}}
-    } else if(favourites) {
-        const show = await fetch(`http://localhost:8080/api/filter/favourites`).then(r => r.json())
-        return {props: {show}}
+    if (search === null && favourites === null) {
+        const shows = await fetch(backendUrl + `page=${page}`).then(r => r.json())
+        return { props: { shows } }
+    } else if (search) {
+        const show = await fetch(backendUrl + `search=${search}`).then(r => r.json())
+        return { props: { show } }
+    } else if (favourites) {
+        const show = await fetch(backendUrl + `filter/favourites`).then(r => r.json())
+        return { props: { show } }
     }
 }
 
-function filter() {
-    let value = document.getElementById("filter").value;
-    fetch("http://localhost:8080/api/filter/" + value)
-        .then((response) => response.json())
-        .then( response => {
-            router.reload(window.location.pathname);})
-}
 
-function buttonFilter(buttonValue) {
-    let value = buttonValue;
-    fetch("http://localhost:8080/api/filter/" + value)
-        .then((response) => response.json())
-        .then( response => {
-            router.reload(window.location.pathname);})
-}
+
+
 
